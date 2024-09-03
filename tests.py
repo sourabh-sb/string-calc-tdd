413b1609fbc1c2b8e033263e501f4eadc84bb80f
sourabh-sb <banerjeesourav60@gmail.com>
2024-09-03 21:32:29 +0530

    handle new lines and multiple numbers

--- tests.py
+++ tests.py
@@ -1,7 +1,16 @@
 from add import add
+
 def test_empty_string():
     assert add("") == 0
+
 def test_one_number():
     assert add("1") == 1
+
 def test_two_numbers():
     assert add("1,5") == 6
+
+def test_add_multiple_numbers():
+    assert add("1,2,3,4") == 10
+
+def test_add_new_lines_between_numbers():
+    assert add("1\n2,3") == 6
